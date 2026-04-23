da4e197e12a02723541f7616329c906e013a6232
bhanuteja <bbhanu41@gmail.com>
2026-04-24 01:08:31 +0530

    credentials-admin

--- CreateAdmin.js
+++ CreateAdmin.js
@@ -5,10 +5,10 @@ const Admin = require("./models/Admin");
 mongoose.connect("mongodb://bhanu:804GnFb9TSFQX5mL@ac-2hdwoa5-shard-00-00.hyz9mzg.mongodb.net:27017,ac-2hdwoa5-shard-00-01.hyz9mzg.mongodb.net:27017,ac-2hdwoa5-shard-00-02.hyz9mzg.mongodb.net:27017/?ssl=true&replicaSet=atlas-9r0xpr-shard-0&authSource=admin&appName=Cluster0");
 
 async function createAdmin() {
-  const hashedPassword = await bcrypt.hash("804GnFb9TSFQX5mL", 10);
+  const hashedPassword = await bcrypt.hash("admin123", 10);
 
   const admin = new Admin({
-    username: "bhanu",
+    username: "admin",
     password: hashedPassword
   });
 
